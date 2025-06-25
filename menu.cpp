8ed243c3be227c105ebaf6c8e72284b9fc5e24cc
João Pedro <jplisboa.dev@gmail.com>
2025-06-25 10:21:09 -0300

    Criação: menu.h e menu.cpp

---
+++ menu.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<string>
+#include "menu.h"
+using namespace std;
+
+int main(){
+  
+}
---
+++ menu.h
@@ -0,0 +1,13 @@
+#ifndef MENU_H
+#define MENU_H
+
+class Menu{
+  public:
+    void mostrarMenuPrincipal();
+    void menuLocais();
+    void menuVeiculos();
+    void menuPedidos();
+};
+
+
+#endif MENU_H
