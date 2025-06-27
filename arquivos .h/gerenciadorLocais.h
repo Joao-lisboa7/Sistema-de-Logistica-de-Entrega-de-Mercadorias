5597dc945b3386bded75bd0efbaa33ba81d5064f
Eli Júnior Dias <elijuniordias098@gmail.com>
2025-06-27 17:53:34 -0300

    Adicionado memória em arquivo binário

--- arquivos .h/gerenciadorLocais.h
+++ arquivos .h/gerenciadorLocais.h
@@ -2,17 +2,25 @@
 #define GERENCIADORLOCAIS_H
 
 #include <vector>
-#include "local.h"
+#include <string>
+#include "local.h" // Certifique-se de que este arquivo define a classe/struct Local
 
 class GerenciadorLocais {
-private:
-    std::vector<Local> locais;
-
 public:
+    // --- Métodos Públicos ---
+    GerenciadorLocais(); // Construtor para carregar os locais do arquivo
     void criarLocal();
     void listarLocais();
     void atualizarLocal();
     void excluirLocal();
+
+private:
+    // --- Atributos e Métodos Privados ---
+    std::vector<Local> locais;
+    const std::string nomeDoArquivo = "locais.dat";
+
+    void carregarLocaisDoArquivo();
+    void salvarLocaisNoArquivo();
 };
 
 #endif // GERENCIADORLOCAIS_H
